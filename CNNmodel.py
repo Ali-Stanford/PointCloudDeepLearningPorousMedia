5b942138df021e15be0910461d3bb8bdd994f760
Ali-Stanford <58951525+Ali-Stanford@users.noreply.github.com>
2023-09-19 07:20:51 -0700

    Update CNNmodel.py

--- CNNmodel.py
+++ CNNmodel.py
@@ -16,14 +16,12 @@
 #  year={2021}, 
 #  publisher={AIP Publishing LLC}}
 
+# First, we need to import necessary libraries
+import numpy as np
 import tensorflow as tf
-from tensorflow.python.keras.layers import Input, Dense
-from tensorflow.python.keras import optimizers
-from tensorflow.python.keras.layers import Input
-from tensorflow.python.keras.models import Model
-from tensorflow.python.keras.layers import Dense, Reshape, Dropout, Flatten  
-from tensorflow.python.keras.layers import Convolution2D, Convolution3D, MaxPooling1D, BatchNormalization
-from tensorflow.python.keras.layers import Lambda, concatenate
+from tensorflow.keras.layers import Input, Dense, Convolution3D, BatchNormalization, Dropout, Flatten
+from tensorflow.keras import optimizers
+from tensorflow.keras.models import Model
 
 class Permeability3DCNN:
     def __init__(self, input_shape, category):
@@ -61,10 +59,17 @@ class Permeability3DCNN:
 #cnn3d_model = Permeability3DCNN(input_shape=(64, 64, 64, 1), category=1)
 #model = cnn3d_model.model 
 
-#model.compile(optimizers.Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=0.000001, decay=0.1)
+#model.compile(optimizers.Adam(learning_rate=0.01, beta_1=0.9, beta_2=0.999, epsilon=0.000001)
 #                   , loss='mean_squared_error', metrics=['mean_squared_error'])
 
-#input_training = zeros([n_training,64,64,64,1],dtype='i')
-#output_training = zeros([n_training],dtype='f') 
+# Generate fake data (just to test the model)
+#n_training = 100  # Set the number of training samples
+#n_validation = 20  # Set the number of validation samples
 
-#results = model.fit(input_training, output_training, batch_size=256, epochs=1500, shuffle=True, verbose=1, validation_split=0.0, validation_data=(input_validation, output_validation))
+#input_training = np.random.randn(n_training, 64, 64, 64, 1)
+#output_training = np.random.randn(n_training)
+
+#input_validation = np.random.randn(n_validation, 64, 64, 64, 1)
+#output_validation = np.random.randn(n_validation)
+
+#results = model.fit(input_training, output_training, batch_size=256, epochs=1500, shuffle=True, verbose=1, validation_data=(input_validation, output_validation))
